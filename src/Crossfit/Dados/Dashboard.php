b0d74a55864af80c4785af09f21c1fddf52852ae
jlmachado1 <joaolmsb@gmail.com>
2014-01-07 10:51:46 -0200

    Pequenos ajustes

--- src/Crossfit/Dados/Dashboard.php
+++ src/Crossfit/Dados/Dashboard.php
@@ -7,7 +7,7 @@ class Dashboard
 {
 	public static function retorna()
 	{
-		$sql = "SELECT (SELECT count(*) FROM aluno WHERE aluno_status = 'A') as ativo, (SELECT count(*) FROM aluno WHERE aluno_status = 'I') as inativo, (SELECT count(*) FROM aluno WHERE aluno_status = 'T') as trancado FROM aluno LIMIT 0,1";
+		$sql = "SELECT (SELECT count(*) FROM contrato WHERE status = 'A') as ativo, (SELECT count(*) FROM contrato WHERE status = 'I') as inativo, (SELECT count(*) FROM contrato WHERE status = 'T') as trancado FROM contrato LIMIT 0,1";
 		$resultado = Conexao::get()->fetchAssoc($sql);
 		return $resultado;
 	}

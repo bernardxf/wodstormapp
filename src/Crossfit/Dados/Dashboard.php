c528c2d4ef811471b53684050d5236adcfa27b42
jlmachado1 <joaolmsb@gmail.com>
2014-01-09 22:45:28 -0200

    pequenos ajustes

--- src/Crossfit/Dados/Dashboard.php
+++ src/Crossfit/Dados/Dashboard.php
@@ -2,13 +2,14 @@
 namespace Crossfit\Dados;
 
 use Crossfit\Conexao;
+use Crossfit\App;
 
 class Dashboard
 {
 	public static function retorna()
 	{
-		$sql = "SELECT (SELECT count(*) FROM contrato WHERE status = 'A') as ativo, (SELECT count(*) FROM contrato WHERE status = 'I') as inativo, (SELECT count(*) FROM contrato WHERE status = 'T') as trancado FROM contrato LIMIT 0,1";
-		$resultado = Conexao::get()->fetchAssoc($sql);
+		$sql = "SELECT (SELECT count(*) FROM contrato WHERE status = 'A' AND id_organizacao = ?) as ativo, (SELECT count(*) FROM contrato WHERE status = 'I' AND id_organizacao = ?) as inativo, (SELECT count(*) FROM contrato WHERE status = 'T' AND id_organizacao = ?) as trancado FROM contrato LIMIT 0,1";
+		$resultado = Conexao::get()->fetchAssoc($sql, array(App::getSession()->get('organizacao'),App::getSession()->get('organizacao'),App::getSession()->get('organizacao')));
 		return $resultado;
 	}
 }

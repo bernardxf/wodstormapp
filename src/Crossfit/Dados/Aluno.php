3b973c789bc1bce7e3466adc0d24d36ce6117780
jlmachado1 <joaolmsb@gmail.com>
2014-01-13 15:00:51 -0200

    não lista na busca de alunos pra presenca os alunos q ja receberam presenca no dia fixes #19

--- src/Crossfit/Dados/Aluno.php
+++ src/Crossfit/Dados/Aluno.php
@@ -19,8 +19,12 @@ class Aluno
 		$sql = "select aluno.id_aluno as id_aluno, aluno.nome as nome, plano.nome as plano, contrato.status as status, contrato.data_fim as data_fim from aluno 
 				join contrato on contrato.id_aluno = aluno.id_aluno
 				join plano on contrato.id_plano = plano.id_plano
-				where aluno.nome like ? AND contrato.status != 'I' AND aluno.id_organizacao = ?";
-		$resultado = Conexao::get()->fetchAll($sql, array('%'.$nome.'%', App::getSession()->get('organizacao')));
+				where aluno.id_aluno NOT IN (
+					select id_aluno from alunos_aula 
+					join aula on aula.id_aula = alunos_aula.id_aula
+					where aula.data = CURDATE() and alunos_aula.id_organizacao = ?
+				) and aluno.nome like ? AND contrato.status != 'I' AND aluno.id_organizacao = ?";
+		$resultado = Conexao::get()->fetchAll($sql, array(App::getSession()->get('organizacao'), '%'.$nome.'%', App::getSession()->get('organizacao')));
 		return $resultado;	
 	}
 

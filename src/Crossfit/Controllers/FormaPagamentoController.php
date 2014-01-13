ce902ce1464909bce9fb727f85094f5d6460b325
jlmachado1 <joaolmsb@gmail.com>
2014-01-13 18:48:05 -0200

    pequenos ajustes em controllers

--- src/Crossfit/Controllers/FormaPagamentoController.php
+++ src/Crossfit/Controllers/FormaPagamentoController.php
@@ -35,7 +35,7 @@ class FormaPagamentoController
 		$response = new Response(); 
 		$formapagamentoDataset = json_decode($request->getContent(), true);
 
-		$formapagamentoDataset = ['id_organizacao'] = App::getSession()->get('organizacao');
+		$formapagamentoDataset['id_organizacao'] = App::getSession()->get('organizacao');
 
 		FormaPagamento::salvaFormaPagamento($formapagamentoDataset);		
 

af010581f6d830992c2c7309198f55e69d487b47
Bernard Faria <bernardxf@gmail.com>
2014-07-12 19:33:33 -0300

    Ajustes no menu de usuario

--- docs/dump/Dump20140708.sql
+++ docs/dump/Dump20140708.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `wodstorm` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `wodstorm`;
 -- MySQL dump 10.13  Distrib 5.5.35, for debian-linux-gnu (i686)
 --
 -- Host: localhost    Database: wodstorm

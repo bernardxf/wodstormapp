7c531b75a9ddad00d2b3c2abe9bd6c05b258b02d
Jhoterdan de Paula <jotaskt@gmail.com>
2014-07-13 03:15:20 -0300

    correção dias trancados

--- public/controllers/appcontrollers.js
+++ public/controllers/appcontrollers.js
@@ -472,16 +472,47 @@ AppControllers.controller('ContratoController', ['$scope', 'ContratoResource', '
 		});
 	};
 
+    function diasDecorridos(strDt1, dt2){
+        // variáveis auxiliares
+        var minuto = 60000;
+        var dia = minuto * 60 * 24;
+        var horarioVerao = 0;
+        var dt1 = new Date (strDt1.split("-").join());
+
+        // ajusta o horario de cada objeto Date
+        dt1.setHours(0);
+        dt1.setMinutes(0);
+        dt1.setSeconds(0);
+        dt2.setHours(0);
+        dt2.setMinutes(0);
+        dt2.setSeconds(0);
+
+        // determina o fuso horário de cada objeto Date
+        var fh1 = dt1.getTimezoneOffset();
+        var fh2 = dt2.getTimezoneOffset();
+
+        // retira a diferença do horário de verão
+        if(dt2 > dt1){
+            horarioVerao = (fh2 - fh1) * minuto;
+        }
+        else{
+            horarioVerao = (fh1 - fh2) * minuto;
+        }
+
+        var dif = Math.abs(dt2.getTime() - dt1.getTime()) - horarioVerao;
+        return Math.ceil(dif / dia);
+    }
 	$scope.atualizaVencimentoContrato = function(){
-		var contrato = $scope.cadContratoDataset,		
+		var contrato = $scope.cadContratoDataset,
 		dtFimComputada = new Date (contrato.data_fim_computada.split("-").join()),
 		qtdDiasTrancados = contrato.dias_trancado,
 		month = [],
-		dtFimAtual = new Date(contrato.data_fim_computada.split("-").join());		
+		dtFimAtual = new Date(contrato.data_fim_computada.split("-").join());
 
-		if(qtdDiasTrancados === undefined || qtdDiasTrancados === "" || qtdDiasTrancados === null){
-			qtdDiasTrancados = 0;				
-		}
+        if(qtdDiasTrancados === undefined || qtdDiasTrancados === "" || qtdDiasTrancados === null){
+            qtdDiasTrancados = diasDecorridos(contrato.data_fim_computada,dtFimAtual);
+        }
+
 		month[0]= "01";
 		month[1]= "02";
 		month[2]= "03";

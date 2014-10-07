0dfbc6b8cd53c0a1e1cb2793813c552201b8bab1
João Luiz <jlmachado@Macbook-Pro.local>
2014-10-07 01:27:01 -0300

    Ajustes query relatorio de metricas

--- src/Crossfit/Dados/Contrato.php
+++ src/Crossfit/Dados/Contrato.php
@@ -67,14 +67,16 @@ class Contrato
 				FROM contrato
 				JOIN aluno on aluno.id_aluno = contrato.id_aluno
 				WHERE contrato.data_inicio <= ?
+				AND aluno.status = ?
 				AND contrato.data_fim >= ?
 				AND contrato.id_organizacao = ?";
-		$ativoPeriodo = Conexao::get()->fetchAll($sql, array($dataFim, $dataFim, $idOrganizacao));
+		$ativoPeriodo = Conexao::get()->fetchAll($sql, array($dataFim, 'A', $dataFim, $idOrganizacao));
 
 		$sql = "SELECT contrato.id_contrato, aluno.id_aluno, aluno.nome
 				FROM contrato
 				JOIN aluno on aluno.id_aluno = contrato.id_aluno
 				WHERE contrato.status = ?
+				AND aluno.status = ?
 				AND contrato.data_fim between ? AND ?
 				AND contrato.id_organizacao = ?
 				AND not exists (
@@ -83,13 +85,14 @@ class Contrato
 					WHERE cont.id_aluno = contrato.id_aluno
 					AND cont.status = ?
 					AND cont.id_organizacao = ?)";
-		$finalizado = Conexao::get()->fetchAll($sql, array('F', $dataInicio, $dataFim, $idOrganizacao, 'A', $idOrganizacao));
+		$finalizado = Conexao::get()->fetchAll($sql, array('F', 'A', $dataInicio, $dataFim, $idOrganizacao, 'A', $idOrganizacao));
 
 		
 		$sql = "SELECT contrato.id_contrato, aluno.id_aluno, aluno.nome
 				FROM contrato
 				JOIN aluno on aluno.id_aluno = contrato.id_aluno
 				WHERE contrato.status = ?
+				AND aluno.status = ?
 				AND contrato.id_organizacao = ?
 				AND contrato.data_inicio between ? AND ?
 				AND exists (
@@ -100,12 +103,13 @@ class Contrato
 					AND cont.data_fim <= ?
 					AND cont.id_organizacao = ?
 					)";
-		$renovado = Conexao::get()->fetchAll($sql, array('A', $idOrganizacao, $dataInicio, $dataFim, 'F', $dataFim, $idOrganizacao));
+		$renovado = Conexao::get()->fetchAll($sql, array('A', 'A', $idOrganizacao, $dataInicio, $dataFim, 'F', $dataFim, $idOrganizacao));
 
 		$sql = "SELECT contrato.id_contrato, aluno.id_aluno, aluno.nome
 				FROM contrato
 				JOIN aluno on aluno.id_aluno = contrato.id_aluno
 				WHERE contrato.status = ?
+				AND aluno.status = ?
 				AND contrato.id_organizacao = ?
 				AND contrato.data_inicio between ? AND ?
 				AND not exists (
@@ -115,14 +119,15 @@ class Contrato
 					AND cont.status in (?, ?)
 					AND cont.id_organizacao = ?
 					)";
-		$novos = Conexao::get()->fetchAll($sql, array('A', $idOrganizacao, $dataInicio, $dataFim, 'F', 'I', $idOrganizacao));
+		$novos = Conexao::get()->fetchAll($sql, array('A', 'A', $idOrganizacao, $dataInicio, $dataFim, 'F', 'I', $idOrganizacao));
 
 		$sql = "SELECT contrato.id_contrato, aluno.id_aluno, aluno.nome
 				FROM contrato
 				JOIN aluno on aluno.id_aluno = contrato.id_aluno
  				WHERE contrato.status = ?
+ 				AND aluno.status = ?
  				AND contrato.id_organizacao = ?";
- 		$ativos = Conexao::get()->fetchAll($sql, array('A', $idOrganizacao));
+ 		$ativos = Conexao::get()->fetchAll($sql, array('A', 'A', $idOrganizacao));
 
 
 		$resultado = compact('ativoPeriodo','finalizado','renovado','novos','ativos');

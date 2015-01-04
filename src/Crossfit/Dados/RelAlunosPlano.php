4f867c73c078bdf8dc37367820822a785d529a37
jlmachado <joaolmsb@gmail.com>
2015-01-04 19:27:45 -0200

    Ajustes relatorio de alunos por plano

--- src/Crossfit/Dados/RelAlunosPlano.php
+++ src/Crossfit/Dados/RelAlunosPlano.php
@@ -12,7 +12,7 @@ class RelAlunosPlano
 		$sql = "select count(DISTINCT(contrato.id_aluno)) as num_alunos, plano.nome as plano, round(count(DISTINCT(contrato.id_aluno)) / (
 					select count(DISTINCT(c.id_aluno)) from contrato as c
 					join aluno  as a on a.id_aluno = c.id_aluno and a.status = 'A'
-					where c.id_organizacao = 1 and c.status in ('A', 'T')
+					where c.id_organizacao = ? and c.status in ('A', 'T')
 				) * 100, 2) as percentual  from contrato 
 				join aluno on aluno.id_aluno = contrato.id_aluno and aluno.status = 'A'
 				join plano on plano.id_plano = contrato.id_plano
@@ -20,7 +20,7 @@ class RelAlunosPlano
 				group by plano.id_plano
 				order by plano.tipo ASC, plano.nome ASC;";
 				
-		$resultado = Conexao::get()->fetchAll($sql, array($idOrganizacao));
+		$resultado = Conexao::get()->fetchAll($sql, array($idOrganizacao, $idOrganizacao));
 
 		return $resultado;
 
